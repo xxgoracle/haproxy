7e121c496619b916cf31db721344df4569d6addf
Willy Tarreau <w@1wt.eu>
2020-05-27 15:59:00 +0200

    REORG: includes: move version.h to haproxy/
    Few files were affected. The release scripts was updated.

--- src/log.c
+++ src/log.c
@@ -27,7 +27,7 @@
 #include <haproxy/base.h>
 #include <common/standard.h>
 #include <common/time.h>
-#include <common/version.h>
+#include <haproxy/version.h>
 
 #include <types/cli.h>
 #include <types/global.h>
