656ce87021e386e7d9e04644219bc8e52b4c99c9
Willy Tarreau <w@1wt.eu>
2020-06-04 20:22:59 +0200

    REORG: includes: move lb_map.h to haproxy/lb_map{,-t}.h
    Nothing was changed.

--- src/lb_map.c
+++ src/lb_map.c
@@ -11,12 +11,12 @@
  */
 
 #include <haproxy/base.h>
+#include <haproxy/lb_map.h>
 #include <import/eb32tree.h>
 
 #include <types/server.h>
 
 #include <proto/backend.h>
-#include <proto/lb_map.h>
 #include <proto/queue.h>
 
 /* this function updates the map according to server <srv>'s new state.
